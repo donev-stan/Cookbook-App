533893b06a892f72935acfde57a9f0ab545927db
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-13 13:29:26 +0430

    add ingredients to shopping list

--- cookbook-app/src/app/shopping-list/shopping-list.component.ts
+++ cookbook-app/src/app/shopping-list/shopping-list.component.ts
@@ -13,6 +13,9 @@ export class ShoppingListComponent implements OnInit {
   ];
 
   constructor() {}
-
   ngOnInit(): void {}
+
+  onIngredientAdded(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+  }
 }
