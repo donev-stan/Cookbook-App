89b8820660d84dd4e9cdabe7dbbd38ed7c473093
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-10-31 15:14:21 +0200

    unsubscribing on component destroy and img preview

--- cookbook-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cookbook-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Recipe } from 'src/app/recipes/recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -10,6 +11,7 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  subscription: Subscription;
 
   constructor(
     private recipeService: RecipeService,
@@ -20,7 +22,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
 
-    this.recipeService.recipesChanged.subscribe({
+    this.subscription = this.recipeService.recipesChanged.subscribe({
       next: (recipes: Recipe[]) => {
         this.recipes = recipes;
       },
@@ -30,5 +32,9 @@ export class RecipeListComponent implements OnInit {
   onNewRecipe(): void {
     // this.router.navigate(['/recipes/new']);
     this.router.navigate(['new'], { relativeTo: this.route });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
