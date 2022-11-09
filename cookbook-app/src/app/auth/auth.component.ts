31a04a1b94d2b38801f28cc242562dd11cfde660
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-11-09 17:04:39 +0200

    routing and dynamic visualization

--- cookbook-app/src/app/auth/auth.component.ts
+++ cookbook-app/src/app/auth/auth.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  isLoginMode: boolean = true;
+
   constructor() {}
 
   ngOnInit() {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
