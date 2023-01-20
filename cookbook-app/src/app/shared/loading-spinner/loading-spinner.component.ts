64faacda05fd2df6d3884fc5b598cdca0d57ed43
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-20 17:02:59 +0200

    loading component update

--- cookbook-app/src/app/shared/loading-spinner/loading-spinner.component.ts
+++ cookbook-app/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
-  styleUrls: ['./loading-spinner.component.css'],
-  template:
-    '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>',
+  template: 'Loading spinner...',
 })
 export class LoadingSpinnerComponent implements OnInit {
   constructor() {}
