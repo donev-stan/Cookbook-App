9dcd9ca76deaac5ea19246215179c48199c074c3
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2023-01-23 14:15:47 +0200

    Adding token to outgoing requests

--- cookbook-app/src/app/shared/data-storage.service.ts
+++ cookbook-app/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { exhaustMap, map, take, tap } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -8,7 +9,11 @@ import { RecipeService } from '../recipes/recipe.service';
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -23,20 +28,46 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>(
-        `https://cookbook---angular-default-rtdb.europe-west1.firebasedatabase.app/recipes.json`
-      )
-      .pipe(
-        map((recipes) => {
-          return recipes.map((recipe) => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
-            };
-          });
-        }),
-        tap((recipes) => this.recipeService.setRecipes(recipes))
-      );
+    // this.authService.user.subscribe().unsubscribe(); --- take would take 1 emitted value and then unsubscribe
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(
+          `https://cookbook---angular-default-rtdb.europe-west1.firebasedatabase.app/recipes.json`,
+          {
+            params: new HttpParams().set('auth', user.token),
+          }
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => this.recipeService.setRecipes(recipes))
+    );
+
+    // exhaustMap ->
+    // We get the data from the first observable (user)
+    // After that we pass a new observable which will replace the previous one
+
+    // return this.http
+    //   .get<Recipe[]>(
+    //     `https://cookbook---angular-default-rtdb.europe-west1.firebasedatabase.app/recipes.json`
+    //   )
+    //   .pipe(
+    //     map((recipes) => {
+    //       return recipes.map((recipe) => {
+    //         return {
+    //           ...recipe,
+    //           ingredients: recipe.ingredients ? recipe.ingredients : [],
+    //         };
+    //       });
+    //     }),
+    //     tap((recipes) => this.recipeService.setRecipes(recipes))
+    //   );
   }
 }
